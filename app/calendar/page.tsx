e67e4ee591aef89c19919c444866d2779844e2ae
pkarlos07 <pkarlos07@gmail.com>
2025-03-01 14:55:15 -0600

    initial framework

---
+++ app/calendar/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+
+export default function App() {
+  return (
+    <DayPicker className="react-day-picker" mode="single"/>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4 w-screen h-1/10 text-center flex items-center justify-center">
+        <h1 className="text-4xl">Calender-Project</h1>
+    </header>
+  );
+};
+
+export default Header;
